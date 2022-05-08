8334d9549ad45943fd8ca3ff0ff8b15cd49a7c98
maxhampel <70974724+maxhampel@users.noreply.github.com>
2022-05-08 17:28:34 -0400

    Create nfragment.kt

---
+++ TheBirdWatcherApp/app/src/main/java/com/example/birdwatcher/ui/notifications/nfragment.kt
@@ -0,0 +1,39 @@
+package com.example.birdwatcher.ui.notifications
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.birdwatcher.NotificationActivity
+import com.example.birdwatcher.databinding.FragmentNotificationsBinding
+
+class nfragment : Fragment() {
+
+    private var _binding: FragmentNotificationsBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val intent = Intent(getActivity(), NotificationActivity::class.java)
+        getActivity()?.startActivity(intent)
+
+        val notificationsViewModel =
+            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+
+        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+
+    }
+}
