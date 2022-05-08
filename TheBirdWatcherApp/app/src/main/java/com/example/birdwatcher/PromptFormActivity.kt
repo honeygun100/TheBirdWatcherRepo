70bf8696357b0d9bd28f36e3b45400e683939727
HosseinAbianeh <habianeh@umd.edu>
2022-05-08 19:30:35 -0400

    kapwd

--- TheBirdWatcherApp/app/src/main/java/com/example/birdwatcher/PromptFormActivity.kt
+++ TheBirdWatcherApp/app/src/main/java/com/example/birdwatcher/PromptFormActivity.kt
@@ -121,6 +121,7 @@ class PromptFormActivity : AppCompatActivity() {
         }
     }
 
+    @Deprecated("Deprecated in Java")
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
