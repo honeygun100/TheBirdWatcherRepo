bde2c2958b2484e061bd2e14dd52e0f314b14615
maxhampel <70974724+maxhampel@users.noreply.github.com>
2022-05-08 17:28:05 -0400

    Update NotificationsViewModel.kt

--- TheBirdWatcherApp/app/src/main/java/com/example/birdwatcher/ui/notifications/NotificationsViewModel.kt
+++ TheBirdWatcherApp/app/src/main/java/com/example/birdwatcher/ui/notifications/NotificationsViewModel.kt
@@ -6,19 +6,10 @@ import com.example.birdwatcher.NotifHelper
 
 class NotificationsViewModel : ViewModel() {
 
-    // private val nHandler = NotifHelper(, null)
 
     val _text = MutableLiveData<MutableList<String>>().apply {
-        //default notifications
-        value = mutableListOf("welcome to bird watching")
+        value = mutableListOf("Welcome to bird watching")
     }
     var text: MutableLiveData<MutableList<String>> = _text
-
-
-    //add notification(s) in arraylist form
-    fun addnotif(n: ArrayList<String>) {
-        _text.value = n.toMutableList()
-        text = _text
-    }
 
 }
